243411f3cf436ed8a5924126c2f17bbb3a0c802c
Daru Nugroho <darunugroho00@gmail.com>
2022-02-02 21:35:22 +0700

    update tampilan card tengah project flutter_goorange_App

--- project_magang/flutter_goorange_app/lib/main.dart
+++ project_magang/flutter_goorange_app/lib/main.dart
@@ -34,9 +34,38 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.white,
         title: const Text(
-          'Goorange',
+          'goorange',
+          style: TextStyle(
+            color: Colors.deepOrange,
+            fontWeight: FontWeight.bold,
+            fontSize: 24.0,
+          ),
         ),
+        actions: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: const <Widget>[
+              Text(
+                'Sandi Fadilah',
+                style: TextStyle(
+                  color: Colors.deepOrange,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Icon(
+                  Icons.person_rounded,
+                  color: Colors.black,
+                  size: 30.0,
+                ),
+              )
+            ],
+          )
+        ],
       ),
       body: Padding(
         padding: EdgeInsets.fromLTRB(3.0, 0.0, 3.0, 3.0),
@@ -54,74 +83,127 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             Column(
               children: <Widget>[
-                Row(
+                const SizedBox(
+                  height: 160.0,
+                ),
+                Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     Container(
-                      margin: EdgeInsets.only(top: 160.0),
+                      //margin: EdgeInsets.only(top: 160.0),
                       width: MediaQuery.of(context).size.width * 0.9,
-                      child: Card(
-                        // card pertama
-                        child: Padding(
-                          padding: EdgeInsets.all(10.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Card(
-                                // card kedua warna oren
-                                child: Container(
-                                  color: Colors.deepOrange[50],
-                                  width:
-                                      MediaQuery.of(context).size.width * 0.8,
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(
-                                      10.0,
-                                    ),
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: <Widget>[
-                                        // Text
-                                        Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.start,
-                                          children: <Widget>[
-                                            Container(
-                                              width: 160.0,
-                                              child: const Text(
-                                                  'Ajak Teman bergabung dan dapatkan banyak kebaikan'),
-                                            )
-                                          ],
-                                        ),
-                                        // Button oren
-                                        Row(
-                                          // Masil Belum yakin Posisi endnya
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.end,
-                                          children: <Widget>[
-                                            RaisedButton(
-                                              child: const Text(
-                                                'Ajak Teman',
+                      decoration: const BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(10.0),
+                              topRight: Radius.circular(10.0))),
+                      child: Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Card(
+                              // card kedua warna oren
+                              child: Container(
+                                color: Colors.deepOrange[50],
+                                width: MediaQuery.of(context).size.width * 0.8,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(
+                                    10.0,
+                                  ),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      // Text
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.start,
+                                        children: <Widget>[
+                                          Container(
+                                            width: 160.0,
+                                            child: const Text(
+                                                'Ajak Teman bergabung dan dapatkan banyak kebaikan',
                                                 style: TextStyle(
-                                                    color: Colors.white,
                                                     fontWeight:
-                                                        FontWeight.bold),
-                                              ),
-                                              onPressed: () {},
-                                              color: Colors.deepOrange[600],
-                                            )
-                                          ],
-                                        )
-                                      ],
-                                    ),
+                                                        FontWeight.bold)),
+                                          )
+                                        ],
+                                      ),
+                                      // Button oren
+                                      Row(
+                                        // Masil Belum yakin Posisi endnya
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.end,
+                                        children: <Widget>[
+                                          RaisedButton(
+                                            child: const Text(
+                                              'Ajak Teman',
+                                              style: TextStyle(
+                                                  color: Colors.white,
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                            onPressed: () {},
+                                            color: Colors.deepOrange[600],
+                                          )
+                                        ],
+                                      )
+                                    ],
                                   ),
                                 ),
-                              )
-                            ],
-                          ),
+                              ),
+                            )
+                          ],
                         ),
                       ),
-                    )
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.9,
+                      height: 50.0,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: const BorderRadius.only(
+                          bottomLeft: Radius.circular(10.0),
+                          bottomRight: Radius.circular(10.0),
+                        ),
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: const <Widget>[
+                                Text('KTA masih dalam proses',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold))
+                              ],
+                            ),
+                            const SizedBox(
+                              width: 30.0,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: <Widget>[
+                                OutlinedButton(
+                                  style: OutlinedButton.styleFrom(
+                                      side:
+                                          BorderSide(color: Colors.deepOrange)),
+                                  child: const Text(
+                                    'KTA Digital',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.deepOrange,
+                                    ),
+                                  ),
+                                  onPressed: () {},
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
                   ],
                 ),
                 Container(
@@ -141,12 +223,14 @@ class _MyHomePageState extends State<MyHomePage> {
                                 Icon(
                                   Icons.chat,
                                   size: 64.0,
+                                  color: Colors.deepOrange,
                                 ),
                                 Text(
                                   'Beri masukkan',
                                   style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 16.0),
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16.0,
+                                  ),
                                 )
                               ],
                             ),
@@ -160,12 +244,14 @@ class _MyHomePageState extends State<MyHomePage> {
                                 Icon(
                                   Icons.card_giftcard,
                                   size: 64.0,
+                                  color: Colors.deepOrange,
                                 ),
                                 Text(
                                   'Hadiah',
                                   style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 16.0),
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16.0,
+                                  ),
                                 )
                               ],
                             ),
@@ -179,6 +265,7 @@ class _MyHomePageState extends State<MyHomePage> {
                                 Icon(
                                   Icons.home,
                                   size: 64.0,
+                                  color: Colors.deepOrange,
                                 ),
                                 Text(
                                   'Kantor',
@@ -197,7 +284,6 @@ class _MyHomePageState extends State<MyHomePage> {
                 Center(
                   child: SizedBox(
                     width: MediaQuery.of(context).size.width * 0.9,
-                    height: MediaQuery.of(context).size.height,
                     child: Card(),
                   ),
                 ),
